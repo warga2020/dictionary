e92ffc79078a6884873bdefeec62f0596f124242
viktor <vik271178@gmail.com>
2020-11-12 15:27:26 +0300

    DICT-3: modified text_processor/text_processor.py - clean_tokens(list)

--- text_processor/text_processor.py
+++ text_processor/text_processor.py
@@ -1,5 +1,13 @@
 def extract_tokens(in_str: str) -> list:
-    """ extract tokens from text """
+    """ split text into tokens """
     return in_str.split()
 
 
+def clean_tokens(tokens_list: list):
+    """ remove non-alphabetic characters from the beginning and end of tokens """
+    for i, token in enumerate(tokens_list):
+        while not token[0].isalpha():
+            token = token[1:]
+        while not token[-1].isalpha():
+            token = token[:-1]
+        tokens_list[i] = token
